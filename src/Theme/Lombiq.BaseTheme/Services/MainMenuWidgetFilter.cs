a0765b07f1fef281ffd5a03e993e34e329b14edf
Roland Márkus <roland.markus@lombiq.com>
2022-06-30 20:26:51 +0200

    Creating sln and basic folders.

---
+++ src/Theme/Lombiq.BaseTheme/Services/MainMenuWidgetFilter.cs
@@ -0,0 +1,53 @@
+﻿using Lombiq.HelpfulExtensions.Extensions.Widgets;
+using Lombiq.HelpfulExtensions.Extensions.Widgets.ViewModels;
+using Lombiq.HelpfulLibraries.OrchardCore.Mvc;
+using Lombiq.HelpfulLibraries.OrchardCore.Navigation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using OrchardCore.DisplayManagement;
+using OrchardCore.DisplayManagement.Layout;
+using OrchardCore.Navigation;
+using System.Threading.Tasks;
+using static Lombiq.BaseTheme.Constants.ZoneNames;
+
+namespace Lombiq.BaseTheme.Services;
+
+public class MainMenuWidgetFilter : WidgetFilterBase<MenuWidgetViewModel>
+{
+    private readonly INavigationManager _navigationManager;
+    private readonly IActionContextAccessor _actionContextAccessor;
+    private readonly ICssClassHolder _cssClassHolder;
+
+    protected override string ZoneName => Navigation;
+    protected override string ViewName => WidgetTypes.MenuWidget;
+    protected override bool FrontEndOnly => true;
+
+    public MainMenuWidgetFilter(
+        IAuthorizationService authorizationService,
+        ILayoutAccessor layoutAccessor,
+        IShapeFactory shapeFactory,
+        INavigationManager navigationManager,
+        IActionContextAccessor actionContextAccessor,
+        ICssClassHolder cssClassHolder)
+        : base(requiredPermission: null, authorizationService, layoutAccessor, shapeFactory)
+    {
+        _navigationManager = navigationManager;
+        _actionContextAccessor = actionContextAccessor;
+        _cssClassHolder = cssClassHolder;
+    }
+
+    protected override async Task<MenuWidgetViewModel> GetViewModelAsync()
+    {
+        // Add the <nav> classes to the zone holder <nav>.
+        _cssClassHolder.AddClassToZone(Navigation, "navbar-expand-md");
+        _cssClassHolder.AddClassToZone(Navigation, "navbar");
+
+        return new()
+        {
+            MenuItems = await _navigationManager.BuildMenuAsync(
+                MainMenuNavigationProviderBase.MainNavigationName,
+                _actionContextAccessor.ActionContext),
+            NoWrapper = true, // The navigation zone is already the wrapper.
+        };
+    }
+}
---
+++ src/Theme/ThirdParty/TheBootstrapTheme/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Bootstrap Theme",
+    Author = "Antoine Griffard",
+    Website = "https://github.com/agriffard/TheBootstrapTheme",
+    Version = "1.0.0",
+    Description = "Bootstrap theme",
+    Tags = new[] { "Bootstrap" }
+)]
