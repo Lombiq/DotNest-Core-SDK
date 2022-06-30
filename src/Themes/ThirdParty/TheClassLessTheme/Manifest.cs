01e2ce1bfddd57ca922b789bc4264d2e774a0675
Roland Márkus <roland.markus@lombiq.com>
2022-06-30 20:15:31 +0200

    Initializing repository.

--- src/Themes/ThirdParty/TheClassLessTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "The ClassLess Theme",
-    Author = "Antoine Griffard",
-    Website = "https://github.com/agriffard/TheClassLessTheme",
-    Version = "1.0.0",
-    Description = "ClassLess theme"
-)]
--- src/Themes/ThirdParty/TheResumeTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "The Resume Theme",
-    Author = "Antoine Griffard",
-    Website = "https://github.com/agriffard/TheResumeTheme",
-    Version = "1.0.0",
-    Description = "Resume theme"
-)]
