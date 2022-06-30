c9c882fbef0d414d70c583d9128a74cc2c44902d
Roland Márkus <roland.markus@lombiq.com>
2022-06-30 18:49:10 +0200

    Restructuring folders and finalizing solution.

--- src/Themes/Lombiq.BaseTheme/Lombiq.BaseTheme.Samples/Constants/FeatureIds.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Lombiq.BaseTheme.Samples.Constants;
-
-public static class FeatureIds
-{
-    public const string Area = "Lombiq.BaseTheme.Samples";
-    public const string BaseThemeSamples = Area;
-}
