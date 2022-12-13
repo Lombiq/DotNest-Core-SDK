bd4c75b0ec35b8be577b457fc82f8925840db394
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-13 11:39:34 +0100

    Common changes

--- src/DotNest.Core.SDK.Web/Program.cs
+++ src/DotNest.Core.SDK.Web/Program.cs
@@ -1,13 +1,16 @@
-using NLog.Web;
+using Lombiq.Hosting.Tenants.Management.Extensions;
+using OrchardCore.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
-builder.Host.UseNLog();
+builder.Host.UseNLogHost();
 
 var configuration = builder.Configuration;
 
-builder.Services.AddOrchardCms(orchardCoreBuilder =>
+builder.Services
+    .AddSingleton(configuration)
+    .AddOrchardCms(orchardCoreBuilder =>
 {
     orchardCoreBuilder
         .AuthorizeApiRequestsIfEnabled(builder.Configuration)
@@ -17,8 +20,17 @@ builder.Services.AddOrchardCms(orchardCoreBuilder =>
 
     if (builder.Environment.IsDevelopment())
     {
-        orchardCoreBuilder.AddSetupFeatures("OrchardCore.AutoSetup");
+        orchardCoreBuilder
+                .AddSetupFeatures("OrchardCore.AutoSetup")
+                .HideRecipesByTagsFromSetup("test", "HideFromSetupScreen");
     }
+
+    // Dependencies need to be added to AddTenantFeatures() explicitly.
+    orchardCoreBuilder.AddTenantFeatures(
+        "DotNest.Hosting.Tenants",
+        Lombiq.Hosting.Tenants.Admin.Login.Constants.FeatureNames.SubTenant,
+        Lombiq.Hosting.Tenants.FeaturesGuard.Constants.FeatureNames.FeaturesGuard,
+        Lombiq.Hosting.Tenants.IdleTenantManagement.Constants.FeatureNames.ShutDownIdleTenants);
 });
 
 var app = builder.Build();
