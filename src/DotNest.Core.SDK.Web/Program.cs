936a923259de8c8b43115fd0d81492d3284bee8f
Krisztián Németh <krisztian.nemeth@lombiq.com>
2022-12-14 14:39:23 +0100

    Fixing indents

--- src/DotNest.Core.SDK.Web/Program.cs
+++ src/DotNest.Core.SDK.Web/Program.cs
@@ -11,27 +11,27 @@ var configuration = builder.Configuration;
 builder.Services
     .AddSingleton(configuration)
     .AddOrchardCms(orchardCoreBuilder =>
-{
-    orchardCoreBuilder
-        .AuthorizeApiRequestsIfEnabled(builder.Configuration)
-        .AllowMiniProfilerOnAdmin()
-        .AddDatabaseShellsConfigurationIfAvailable(configuration)
-        .ConfigureSmtpSettings(overrideAdminSettings: false);
-
-    if (builder.Environment.IsDevelopment())
     {
         orchardCoreBuilder
+            .AuthorizeApiRequestsIfEnabled(builder.Configuration)
+            .AllowMiniProfilerOnAdmin()
+            .AddDatabaseShellsConfigurationIfAvailable(configuration)
+            .ConfigureSmtpSettings(overrideAdminSettings: false);
+
+        if (builder.Environment.IsDevelopment())
+        {
+            orchardCoreBuilder
                 .AddSetupFeatures("OrchardCore.AutoSetup")
                 .HideRecipesByTagsFromSetup("test", "HideFromSetupScreen");
-    }
+        }
 
-    // Dependencies need to be added to AddTenantFeatures() explicitly.
-    orchardCoreBuilder.AddTenantFeatures(
-        "DotNest.Hosting.Tenants",
-        Lombiq.Hosting.Tenants.Admin.Login.Constants.FeatureNames.SubTenant,
-        Lombiq.Hosting.Tenants.FeaturesGuard.Constants.FeatureNames.FeaturesGuard,
-        Lombiq.Hosting.Tenants.IdleTenantManagement.Constants.FeatureNames.ShutDownIdleTenants);
-});
+        // Dependencies need to be added to AddTenantFeatures() explicitly.
+        orchardCoreBuilder.AddTenantFeatures(
+            "DotNest.Hosting.Tenants",
+            Lombiq.Hosting.Tenants.Admin.Login.Constants.FeatureNames.SubTenant,
+            Lombiq.Hosting.Tenants.FeaturesGuard.Constants.FeatureNames.FeaturesGuard,
+            Lombiq.Hosting.Tenants.IdleTenantManagement.Constants.FeatureNames.ShutDownIdleTenants);
+    });
 
 var app = builder.Build();
 
