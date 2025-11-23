e06510a665efff4e0f7b7ec24d7660449f3f6069
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-11-23 19:12:53 +0100

    Revert "Fixing test"
    This reverts commit 5a04a01c30397e74c232a129f51d498ccb548de4.

--- test/DotNest.Core.SDK.Tests.UI/Tests/BasicOrchardFeaturesTests.cs
+++ test/DotNest.Core.SDK.Tests.UI/Tests/BasicOrchardFeaturesTests.cs
@@ -1,9 +1,6 @@
 using DotNest.Core.SDK.Tests.UI.Constants;
 using Lombiq.Tests.UI.BasicOrchardFeaturesTesting;
-using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Models;
-using Shouldly;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -18,26 +15,10 @@ public class BasicOrchardFeaturesTests : UITestBase
 
     [Fact]
     public Task BasicOrchardFeaturesShouldWork() =>
-        ExecuteTestAsync(
-            context => context.TestBasicOrchardFeaturesAsync(new OrchardCoreSetupParameters(context)
-            {
-                RecipeId = Recipes.DefaultRecipeId,
-                SkipFrontend = true,
-                SkipRegistration = true,
-            }),
-            configuration =>
-            {
-                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
-                    validationResult =>
-                    {
-                        // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/18510. Can be removed
-                        // once resolved.
-                        var errors = validationResult.GetParsedErrors()
-                            .Where(error => error.RuleId is not "aria-label-misuse");
-                        errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
-                        return Task.CompletedTask;
-                    };
-
-                return Task.CompletedTask;
-            });
+        ExecuteTestAsync(context => context.TestBasicOrchardFeaturesAsync(new OrchardCoreSetupParameters(context)
+        {
+            RecipeId = Recipes.DefaultRecipeId,
+            SkipFrontend = true,
+            SkipRegistration = true,
+        }));
 }
