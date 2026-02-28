5b23da7c4b8e3c3e0b97c55bfe1539b49a13e9f9
Sára El-Saig <sara.el-saig@lombiq.com>
2026-02-28 11:46:41 +0100

    remove obsolete code

--- test/DotNest.Core.SDK.Tests.UI/Tests/BasicOrchardFeaturesTests.cs
+++ test/DotNest.Core.SDK.Tests.UI/Tests/BasicOrchardFeaturesTests.cs
@@ -1,9 +1,6 @@
 using DotNest.Core.SDK.Tests.UI.Constants;
 using Lombiq.Tests.UI.BasicOrchardFeaturesTesting;
-using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Models;
-using Shouldly;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -24,20 +21,5 @@ public class BasicOrchardFeaturesTests : UITestBase
                 RecipeId = Recipes.DefaultRecipeId,
                 SkipFrontend = true,
                 SkipRegistration = true,
-            }),
-            configuration =>
-            {
-                configuration.HtmlValidationConfiguration.AssertHtmlValidationResultAsync =
-                    validationResult =>
-                    {
-                        // Error filtering due to https://github.com/OrchardCMS/OrchardCore/issues/18510. Can be removed
-                        // once resolved.
-                        var errors = validationResult.GetParsedErrors()
-                            .Where(error => error.RuleId is not "aria-label-misuse");
-                        errors.ShouldBeEmpty(HtmlValidationResultExtensions.GetParsedErrorMessageString(errors));
-                        return Task.CompletedTask;
-                    };
-
-                return Task.CompletedTask;
-            });
+            }));
 }
